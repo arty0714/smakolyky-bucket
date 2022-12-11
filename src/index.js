48811a1cd5bb877e96b7bf482155163063d1cd22
Artem <arty071402@gmail.com>
2022-12-11 17:43:02 +0200

    add api to delete project image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import fs from 'fs';
 import { projectUpload } from './utils/multer.js';
 
 dotenv.config();
@@ -16,6 +17,22 @@ app.post('/api/project', projectUpload.single('image'), (req, res) => {
 		url,
 		name: req.file.filename
 	} });
+})
+app.delete('/api/project/:name', (req, res, next) => {
+	const filename = req.params.name;
+	const dir = './projects';
+	let error = null;
+
+	fs.unlink(`${dir}/${filename}`, (err) => {
+		error = err;
+	});
+
+	if (error) {
+		res.status(400).send(err);
+		return;
+	}
+
+	res.status(200).send('success');
 })
 app.post('/api/chapter', (req, res) => {})
 
