6b9dd436f63d5e044ac4e575e2d3c0e6e0835a62
arty0714 <arty071402@gmail.com>
2023-08-27 13:31:03 +0300

    set up environment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:latest
 WORKDIR /app/bucket
+RUN mkdir chapters projects
 COPY package.json .
 RUN npm install
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use('/projects', express.static('projects'))
 app.use('/chapters', express.static('chapters'))
 
 app.post('/api/project', projectUpload.single('image'), (req, res) => {
-	const url = `https://${process.env.HOST}/${req.file.path}`
+	const url = `http://${process.env.VITE_BUCKET_HOST}/${req.file.path}`
 
 	res.status(200).json({ imageInfo: {
 		url,
@@ -39,7 +39,7 @@ app.delete('/api/project/:name', (req, res, next) => {
 })
 app.post('/api/chapter', chapterUpload.single('image'), (req, res) => {
 	const image = req.file;
-	const url = `https://${process.env.HOST}/${image.path}`;
+	const url = `http://${process.env.VITE_BUCKET_HOST}/${image.path}`;
 	const name = image.filename;
 	
 	const imageInfo = {
@@ -67,4 +67,4 @@ app.delete('/api/chapter/:name', (req, res, next) => {
 	res.status(200).send('success');
 })
 
-app.listen(process.env.PORT, console.log(`bucket is up at port ${process.env.PORT}`));
+app.listen(process.env.BUCKET_PORT, console.log(`bucket is up at port ${process.env.PORT}`));
